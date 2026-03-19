3dc2b3dbcf12e80d7176a356116339f3220c8dab
Oleh Papka <olegpapka2@gmail.com>
2026-03-18 22:54:51 -0400

    feat: add basic models for budget app

--- two_coins/settings.py
+++ two_coins/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
     'users.apps.UsersConfig',
+    'budget.apps.BudgetConfig',
 ]
 
 MIDDLEWARE = [
