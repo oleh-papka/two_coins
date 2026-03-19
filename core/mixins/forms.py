67ba7cee906fe14773cdb3ec40a2a72f65f29743
Oleh Papka <olegpapka2@gmail.com>
2026-03-18 21:07:48 -0400

    refactor: widgets setup for form fields

---
+++ core/mixins/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+
+class BootstrapFormMixin:
+        def _get_widget_class(self, field):
+            if isinstance(field.widget, forms.CheckboxInput):
+                return "form-check-input"
+            if isinstance(field.widget, forms.Select):
+                return "form-select"
+            return "form-control"
+
+        def _init_bootstrap(self):
+            for name, field in self.fields.items():
+                existing = field.widget.attrs.get("class", "")
+                widget_class = self._get_widget_class(field)
+
+                error_class = " is-invalid" if name in self.errors else ""
+                field.widget.attrs["class"] = f"{existing} {widget_class}{error_class}".strip()
+                field.widget.attrs.setdefault("placeholder", field.label)
--- users/forms.py
+++ users/forms.py
@@ -1,17 +1,33 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+from core.mixins.forms import BootstrapFormMixin
 from users.models import User
 
 
-class UserAddForm(UserCreationForm):
+class UserAddForm(BootstrapFormMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_bootstrap()
 
-class CustomAuthenticationForm(AuthenticationForm):
+        self.fields["email"].widget.attrs["autocomplete"] = "email"
+        self.fields["password1"].widget.attrs["autocomplete"] = "new-password"
+        self.fields["password2"].widget.attrs["autocomplete"] = "new-password"
+
+
+class CustomAuthenticationForm(BootstrapFormMixin, AuthenticationForm):
     username = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'autofocus': True}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_bootstrap()
+
+        self.fields["username"].widget.attrs["autocomplete"] = "email"
+        self.fields["password"].widget.attrs["autocomplete"] = "current-password"
 
 
 class UserForm(forms.ModelForm):
