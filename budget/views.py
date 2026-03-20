2ad76f1aadd9b6a90045f88253603c944018c8af
Oleh Papka <olegpapka2@gmail.com>
2026-03-19 20:54:56 -0400

    feat: add base dashboard view + index.html

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, TemplateView
 
-# Create your views here.
+from budget.models import Account
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    login_url = 'login'
+    template_name = 'index.html'
+
+
+class AccountListView(LoginRequiredMixin, ListView):
+    login_url = 'login'
+    model = Account
+    template_name = ''
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
