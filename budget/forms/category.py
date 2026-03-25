7f92f1cb2ecb1becf37061ea13c15847e83c06c8
Oleh Papka <olegpapka2@gmail.com>
2026-03-25 16:42:33 -0400

    add: color swatch selector - add color render

--- budget/forms/category.py
+++ budget/forms/category.py
@@ -7,7 +7,7 @@ from core.mixins.forms import BootstrapFormMixin
 class CategoryForm(BootstrapFormMixin, forms.ModelForm):
     class Meta:
         model = Category
-        fields = ('name', 'category_type', 'icon')
+        fields = ('name', 'category_type', 'icon', 'color')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- core/models.py
+++ core/models.py
@@ -9,13 +9,28 @@ class TimeStampMixin(models.Model):
         abstract = True
 
 
+class ColorChoice(models.TextChoices):
+    TRANSPARENT = "bg-body", "Transparent"
+    PRIMARY_SUBTLE = "bg-primary-subtle", "Acid green light"
+    GREEN = "bg-success", "Green"
+    GREEN_SUBTLE = "bg-success-subtle", "Green subtle"
+    RED = "bg-danger", "Red"
+    RED_SUBTLE = "bg-danger-subtle", "Red subtle"
+    YELLOW = "bg-warning", "Yellow"
+    YELLOW_SUBTLE = "bg-warning-subtle", "Yellow subtle"
+    BLUE = "bg-info", "Blue"
+    BLUE_SUBTLE = "bg-info-subtle", "Blue subtle"
+    GRAY = "bg-light", "Gray"
+    BLACK = "bg-dark-subtle", "Black"
+
+
 class StyleMixin(models.Model):
     color = models.CharField(null=False,
                              blank=True,
-                             max_length=6,
-                             default="4caf50",
-                             verbose_name="Color",
-                             help_text="Color in HEX format")
+                             max_length=25,
+                             default=ColorChoice.TRANSPARENT,
+                             choices=ColorChoice,
+                             verbose_name="Color")
     icon = models.CharField(null=True,
                             blank=True,
                             max_length=10,
