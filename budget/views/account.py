b109ea786cbabbdc69b53a3c2c81e96d18625938
Oleh Papka <olegpapka2@gmail.com>
2026-03-29 12:50:11 -0400

    feat: add doughnut charts to account list

--- budget/views/account.py
+++ budget/views/account.py
@@ -1,7 +1,9 @@
+import json
+from collections import defaultdict
 from decimal import Decimal
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Sum, Q
+from django.db.models import Sum, Q, F
 from django.db.models.functions import Coalesce
 from django.urls import reverse_lazy
 from django.utils import timezone
@@ -43,6 +45,42 @@ class AccountDetailView(LoginRequiredMixin, DetailView):
 class AccountListView(ListMixin):
     model = Account
     template_name = 'accounts_list.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        totals = list(
+            self.object_list.values(
+                currency_symbol=F('currency__symbol'),
+                currency_name=F('currency__name'),
+                currency_abbr=F('currency__abbr')
+            ).annotate(total_balance=Sum('balance'))
+        )
+
+        grouped_by_currency = defaultdict(list)
+        for account in self.object_list:
+            grouped_by_currency[account.currency.abbr].append(account)
+
+        chart_data_map = {}
+        for abbr, accounts in grouped_by_currency.items():
+            if len(accounts) <= 1:
+                chart_data_map[abbr] = []
+                continue
+
+            chart_data_map[abbr] = [
+                {
+                    'value': float(account.balance or 0),
+                    'name': account.name,
+                }
+                for account in accounts
+            ]
+
+        for item in totals:
+            item['total_balance'] = float(item['total_balance'])
+            item['chart_data'] = chart_data_map.get(item['currency_abbr'], [])
+
+        ctx['total_by_currency'] = totals
+        return ctx
 
 
 class AccountCreateView(CreateMixin):
